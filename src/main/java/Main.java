3a34dd1a07c136856455eac3139a52fda8cb2408
EvgeniyTLT <Toy444@ya.ru>
2022-06-08 12:11:54 +0300

    add

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Start");
+        Thread thread = new MyThread();
+        thread.start();
+        System.out.println("\nFinish");
+    }
+}
