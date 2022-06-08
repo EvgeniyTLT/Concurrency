df02d45ceeece36108e0df3c55e38d8c605742c7
EvgeniyTLT <Toy444@ya.ru>
2022-06-08 18:03:43 +0300

    add

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,14 +5,31 @@ public class Main {
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 0; i < 1000; i++) {
-                    System.out.print(i);
+                try {
+                    for (int i = 0; i < 1000; i++) {
+                        if (Thread.currentThread().isInterrupted()) {
+                            break;
+                        }
+                        System.out.print(i);
+                        Thread.sleep(1000);
+
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             }
         });
 
         thread.start();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         for (int i = 0; i < 1000; i++) {
+            if (i == 100) {
+                thread.interrupt();
+            }
             System.out.print("M");
         }
         System.out.println("\nFinish");
