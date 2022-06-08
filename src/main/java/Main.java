cd47a109e3da6002624a83deeee36a49b478d17e
EvgeniyTLT <Toy444@ya.ru>
2022-06-08 18:24:52 +0300

    add

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,50 +2,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-        List<Integer> list1 = new ArrayList<>();
-        List<Integer> list2 = new ArrayList<>();
-        List<Integer> list3 = new ArrayList<>();
 
+    private static List<String> list;
+
+    public static void main(String[] args) {
         Thread thread1 = new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 0; i < 1000000; i++) {
-                    list1.add(i);
-                }
+                list = new ArrayList<>();
             }
+
         });
 
         Thread thread2 = new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 0; i < 1000000; i++) {
-                    list2.add(i);
+                long before = System.currentTimeMillis();
+                try {
+                    thread1.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
+                System.out.println(list.size());
+                long after = System.currentTimeMillis();
+                System.out.println(after-before);
             }
         });
-
-        Thread thread3 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 1000000; i++) {
-                    list3.add(i);
-                }
-            }
-        });
-
         thread1.start();
         thread2.start();
-        thread3.start();
-        try {
-            thread1.join();
-            thread2.join();
-            thread3.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        System.out.println(list1.size());
-        System.out.println(list2.size());
-        System.out.println(list3.size());
     }
 }
