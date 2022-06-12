df30b8fa2100a061a73b6e04cf4028cfd8f51fea
EvgeniyTLT <Toy444@ya.ru>
2022-06-12 22:10:29 +0300

    add

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,14 +2,18 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
 
     public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
         CountDownLatch countDownLatch = new CountDownLatch(10);
         for (int i = 0; i < 10; i++) {
             final int index = i;
-            new Thread(new Runnable() {
+            executorService.execute(new Runnable() {
                 @Override
                 public void run() {
                     System.out.println("Start - " + index);
@@ -19,14 +23,15 @@ public class Main {
                         e.printStackTrace();
                     }
                     System.out.println("Finish" + index);
+                    countDownLatch.countDown();
                 }
-            }).start();
-            try {
-                countDownLatch.await();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            countDownLatch.countDown();
+            });
+         }
+        executorService.shutdown();
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
         System.out.println("all threads  are terminated");
     }
