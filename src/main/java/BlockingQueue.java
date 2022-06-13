500a1166b47db8ed5a15d30903de9e3a2b43e0a2
EvgeniyTLT <Toy444@ya.ru>
2022-06-13 23:50:05 +0300

    add

--- src/main/java/BlockingQueue.java
+++ src/main/java/BlockingQueue.java
@@ -1,2 +1,15 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BlockingQueue {
+
+    public Queue<Runnable> queue = new LinkedList<>();
+
+    public void add(Runnable task) {
+        queue.add(task);
+    }
+
+    public Runnable take() {
+        return queue.poll();
+    }
 }
