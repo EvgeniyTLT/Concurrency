5fe4694ae9349ec1bd0345c5a8edd8a9608b387f
EvgeniyTLT <Toy444@ya.ru>
2022-06-15 15:29:52 +0300

    add

--- src/main/java/Main9.java
+++ src/main/java/Main9.java
@@ -1,11 +1,8 @@
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Semaphore;
+import java.util.concurrent.*;
 
 public class Main9 {
     public static void main(String[] args) {
-        CountDownLatch countDownLatch = new CountDownLatch(10);
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(2);
         for (int i = 0; i < 10; i++) {
             new Thread(new Runnable() {
                 @Override
@@ -16,15 +13,19 @@ public class Main9 {
                     try {
                         Thread.sleep(millis);
                         System.out.println(name+ ": Data is ready");
-                        countDownLatch.countDown();
+
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
+
                     try {
-                        countDownLatch.await();
+                        cyclicBarrier.await();
                     } catch (InterruptedException e) {
                         e.printStackTrace();
+                    } catch (BrokenBarrierException e) {
+                        e.printStackTrace();
                     }
+
                     System.out.println(name + ": continue work");
                 }
             }).start();
